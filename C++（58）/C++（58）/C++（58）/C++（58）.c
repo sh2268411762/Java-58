285b6c8dc893c2c66c1701bed7a5edf826d176f3
sh2268411762 <2268411762@qq.com>
2020-09-10 23:19:45 +0800

    C++

---
+++ C++（58）/C++（58）/C++（58）/C++（58）.c
@@ -0,0 +1,28 @@
+﻿//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//
+//void f(int a[])
+//{
+//	printf("%d\n", sizeof(a));
+//}
+//int main()
+//{
+//	char s[] = "Welcome to Bit";
+//	int a[] = { 1,2,3,4,5,6,7,8,9 };
+//	char* p = s;
+//	int n;
+//
+//	printf("len = %d\n", strlen(s));
+//	printf("size =%d\n", sizeof(s));
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n", sizeof(p));
+//	printf("%d\n", sizeof(n));
+//
+//	f(a);  //数组降维成指针
+//	//14  15  36  4 4 4
+//	short* par[10][10];
+//	printf("%d\n", sizeof(par));
+//	return EXIT_SUCCESS;
+//}
